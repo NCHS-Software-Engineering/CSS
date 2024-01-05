aaef8c06e75fcd6817136bc29021370c840c7da0
UnityUser525 <mkmihailov@stu.naperville203.org>
2024-01-05 16:44:40 -0600

    Modified Server
    The Server now sends the client the schedule for the day upon connection or the day changing.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
-// ----- Imports
+// ----- Imports & Initializations
+
 
 const WebSocket = require("ws"); // import websockets
 const FileSystem = require("fs"); // import filesystem
+const Scheduler = require("node-schedule"); // import 
+
+const connections = new Set(); // A set containing all of the client sockets
+var today; // today's schedule
 
 
-// ----- Set up the files -----
+// ----- File Managment -----
 
 
 // make sure that the "files" directory exists
@@ -12,50 +17,110 @@ if (FileSystem.existsSync("files") === false) // make sure the files directory e
 {
     FileSystem.mkdirSync("files");
 }
-// make sure that all of the important files storeing the Scheduals, Calendar, etc. exist
-if (FileSystem.existsSync("files/schedules") === false) // create an empty dictionary of all schedules
+// make sure that all of the important files storeing the Schedules, Calendar, etc. exist
+if (FileSystem.existsSync("files/schedules.json") === false) // create an empty dictionary of all schedules
 {
     const data = {};
     FileSystem.writeFileSync("files/schedules.json", JSON.stringify(data));
 }
-if (FileSystem.existsSync("files/defaultWeek") === false) // create a dictionary matching the day to the schedule
+if (FileSystem.existsSync("files/defaultWeek.json") === false) // create a dictionary matching the day to the schedule
 {
-    const data = {Monday : null, Tuesday : null, Wednesday : null, Thursday : null, Friday : null, Saturday : null, Sunday : null};
-    FileSystem.writeFileSync("files/defaultWeek", JSON.stringify(data));
+    const data = {0 : null, 1 : null, 2 : null, 3 : null, 4 : null, 5 : null, 6 : null}; // Sunday - Saturday : 0 - 6
+    FileSystem.writeFileSync("files/defaultWeek.json", JSON.stringify(data));
 }
-if (FileSystem.existsSync("files/calendar") === false) // create an empty list of all upcoming special schedules
+if (FileSystem.existsSync("files/calendar.json") === false) // create an empty dictionary of all dates
 {
-    const data = [];
-    FileSystem.writeFileSync("files/calendar", JSON.stringify(data));
+    const data = {};
+    FileSystem.writeFileSync("files/calendar.json", JSON.stringify(data));
 }
 
+function getCurrentSchedule(callback)
+{    
+    // figure out todays schedule
 
-// ----- WebSockets Stuff -----
+    const calendar = JSON.parse(FileSystem.readFileSync("files/calendar.json"));
+    const schedules = JSON.parse(FileSystem.readFileSync("files/schedules.json"));
+
+    var dateKey;
+    var scheduleKey;
+
+    const tempDate = new Date();
+    dateKey = tempDate.getMonth() * 100;
+    dateKey += tempDate.getDay();
+    
+    if (calendar[dateKey]) // check special schedules first
+    { 
+        scheduleKey = calendar[dateKey]['schedule'];
+    }
+    else // check the default schedules
+    {
+        const defaultWeek = JSON.parse(FileSystem.readFileSync("files/defaultWeek.json"));
+        
+        scheduleKey = defaultWeek[tempDate.getDay()];
+    }
+
+    const res = schedules[scheduleKey];
+
+    callback(res);
+
+    // if yesterday's schedule was a special one time schedule, delete it // TODO: Testing Needed !!!
+
+    // get yesterday's date info
+    tempDate.setDate(tempDate.getDate() - 1);
+    dateKey = tempDate.getMonth() * 100;
+    dateKey += tempDate.getDay();
+
+    if (calendar[dateKey] && calendar[dateKey]['repeating'] === false)
+    {
+        delete calendar[dateKey];
+        FileSystem.writeFileSync("files/calendar.json", JSON.stringify(calendar)); // replace the calendar file with the updated calendar
+    }
+}
+
+getCurrentSchedule((sch) => {today = sch;}); // initialize today's schedule
+
+
+// ----- WebSocket Managment -----
+
 
 const portNum = 8000; // Change this number in order to change which port the server is listening on
 const wss = new WebSocket.Server({port : portNum}); // represents the server socket
 
-const connections = new Set(); // A set containing all of the client sockets
-// send information to all connections
-async function broadCast()
+async function broadCast() // send information to all connections
 {
     connections.forEach(ws => {
-        ws.send("Server Connection Count: " + connections.size);
+        updateClient(ws);
     });
+}
+async function updateClient(ws) // send information to an individual client
+{
+    ws.send(today);
 }
 
 // runs when a new client connects
 wss.on('connection', (ws) => 
 {
-    connections.add(ws); // store the client socket
+    updateClient(ws); // send the new client the schedule
 
-    broadCast();
+    connections.add(ws); // store the client socket
 
     // runs when a client disconnects
     ws.on('close', () => 
     {
         connections.delete(ws); // remove the client socket
-
-        broadCast();
     }); 
-}); 
+});
+
+
+// ----- Scheduling -----
+
+
+// every day at midnight ...
+const job = Scheduler.scheduleJob("0 0 0 0 0", () =>
+{
+    getCurrentSchedule((sch) => 
+    {
+        today = sch; // reset today's schedule (it's a new day)
+        broadCast(); // broadCast the changes to any connected clients
+    });
+});
