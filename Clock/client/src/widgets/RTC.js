54157c8dd7c3b8d47012c96e2159814c3a73244a
skybound0532 <tianchenggu@outlook.com>
2024-02-13 14:12:34 -0600

    Migrate client to widget system

--- Clock/client/src/widgets/RTC.js
+++ Clock/client/src/widgets/RTC.js
@@ -3,6 +3,20 @@ import React, { useState, useEffect, useRef} from "react";
 
 function RTC()
 {
-    const tempDate = new Date();
-    setCountdown(tempDate.getHours().toString().padStart(2, "0") + ":" + tempDate.getMinutes().toString().padStart(2, "0") + ":" + tempDate.getSeconds().toString().padStart(2, "0"));
+    const [time, setTime] = useState();
+
+    useEffect(() => {
+        const interval =  setInterval(() => {
+                const tempDate = new Date();
+                setTime(tempDate.getHours().toString().padStart(2, "0") + ":" + tempDate.getMinutes().toString().padStart(2, "0") + ":" + tempDate.getSeconds().toString().padStart(2, "0"));
+            }, 100);
+    }, []);
+
+    return(
+        <div>
+            <p>{time}</p>
+        </div>
+    )    
 }
+
+export default RTC;
