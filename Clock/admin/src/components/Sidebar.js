353c7bbf020d1c3f49f8b6eb6c60a544698d2f8a
skybound0532 <tianchenggu@outlook.com>
2024-03-13 14:23:53 -0500

    Create sidebar drawer

--- Clock/admin/src/components/Sidebar.js
+++ Clock/admin/src/components/Sidebar.js
@@ -1,11 +1,40 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
 import ImageRoundedIcon from '@mui/icons-material/ImageRounded';
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import CalendarTodayRoundedIcon from '@mui/icons-material/CalendarTodayRounded';
 import ScheduleRoundedIcon from '@mui/icons-material/ScheduleRounded';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import DateRangeRoundedIcon from '@mui/icons-material/DateRangeRounded';
-import { Box, Divider, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Drawer } from '@mui/material';
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import MenuIcon from '@mui/icons-material/Menu';
+import MuiDrawer from '@mui/material/Drawer';
+import { Box, Divider, List, ListItem, ListItemButton, ListItemText, ListItemIcon, IconButton } from '@mui/material';
+
+/**/
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+  });
+  
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+  });
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
     ({ theme, open }) => ({
@@ -23,12 +52,40 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
       }),
     }),
   );
+/**/
 
 function Sidebar(props) // props.width 
 {
+    const [open, setOpen] = useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    
+    //sx={{width: props.width, paddingTop: 1, marginRight: 1}}
+
     return (
-        <Box sx={{width: props.width, paddingTop: 1, borderRight: 1, marginRight: 1}}>
-            <Drawer>
+        <Box >
+            <Drawer variant="permanent" open={open}>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{
+                    marginRight: 5,
+                    ...(open && { display: 'none' }),
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronLeftRoundedIcon/>
+                </IconButton>
                 <List>
                     <ListItem disablePadding>
                         <Link to="/preview" style={{width: "100%"}}>
